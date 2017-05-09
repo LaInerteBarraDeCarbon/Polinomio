7cafef955859994a68905215b6d3d4c6f54ffb0a
Oc Frutos <o_frutos@yahoo.com>
2017-05-09 20:31:00 -0300

    El comienzo

---
+++ src/polinomio/Polinomio.java
@@ -0,0 +1,38 @@
+package polinomio;
+
+public class Polinomio {
+	private int grado;
+	private double[] coeficientes;
+	
+	public Polinomio{
+		
+	}
+	
+	double evaluarMSucesivas(double x){
+		
+	}
+	
+	double evaluarRecursiva (double x){
+		
+	}
+	
+	double evaluarRecursivaPar (double x){
+		
+	}
+	
+	double evaluarProgDinamica (double x){
+		
+	}
+	
+	double evaluarMejorada (double x){
+		
+	}
+	
+	double evaluarPow (double x){
+		
+	}
+	
+	double evaluarHorner (double x){
+		
+	}
+}
