0b700d35afe97eca07930965f64ba0efab921caf
Pablo Sebastián García <pablosebas33@gmail.com>
2017-05-11 21:20:11 -0300

    Revert "Constructores"
    This reverts commit 9c71c6edbe91c8a75fd29d00fc9025a65992a17f.

--- src/coeficientes/Polinomio.java
+++ src/coeficientes/Polinomio.java
@@ -1,9 +1,5 @@
 package coeficientes;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class Polinomio {
 	private int grado;
 	private double[] coeficientes;
@@ -16,19 +12,22 @@ public class Polinomio {
 	}
 
 	// Constructor a partir de un vector
-	public Polinomio(String arg) {
-		Scanner sc = null;
-		try {
-			sc = new Scanner(new File(arg));
-			this.grado = sc.nextInt();
-			this.coeficientes = new double[grado + 1]; // por el termino independiente
-			for (int i = 0; i < grado + 1; i++) {
-				coeficientes[i] = sc.nextDouble();
+	public Polinomio(double[] vector) {
+		// Si el vector no tiene elementos el usar este constructor equivaldrá a
+		// utilizar el constructor por defecto.
+		if (vector.length == 0) {
+			coeficientes = new double[0];
+			coeficientes[0] = 0;
+			grado = 0;
+		} else {
+			coeficientes = new double[vector.length];
+			grado = vector.length - 1;
+			for (int i = 0; i < vector.length; i++) {
+				coeficientes[i] = vector[i];
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+
 		}
-		sc.close();
+
 	}
 
 	double evaluarMSucesivas(double x) {
